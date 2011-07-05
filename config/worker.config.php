3dc892d45b60b5e876aa48f79e59a452f9b637e0
josh <josh@6b763d2d-d01a-451f-aa8f-a39ca3e19446>
2011-07-05 08:25:04 +0000

    added worker config file [deploy: ec2-scraper]

---
+++ config/worker.config.php
@@ -0,0 +1,44 @@
+<?php  if(!defined('HUB')) exit('No direct script access allowed\n');
+
+	// ******************************* INFORMATION ******************************//
+	
+	// **************************************************************************//
+	//  
+	// ** SETTINGS - All of the settings that are generally constant and only 
+	// ** need to be changed if relocating server or changing database structure
+	// ** 
+	// ** @author	Joshua Heiland <thezenman@gmail.com>
+	// ** @date	 2011-04-22
+	// ** @access	private
+	// ** @param	
+	// ** @return	constants for application 
+	//  	
+	// ***************************************************************************//
+	
+	// ********************************** START **********************************//   
+
+	// ===========================================================================// 
+	// ! Passed arguments through CLI ($argv[1] used for controller)              //
+	// ===========================================================================//    
+	
+	// Amount of keywords to scrape
+	define("KEYWORD_AMOUNT", 1000);
+		    
+	// Avoid potential runaway scripts by limiting max execution time      
+	define("MAX_EXECUTION_TIME", 300);
+	
+	// What search engine to scrape
+	define("ENGINE", 'google');
+	
+	// What search engine to scrape
+	define("ENGINE", 'hourly');
+	
+	// Scrape just new keywords for above engine if set to true
+	define("NEW_KEYWORDS", $argv[5]);  
+	
+	// Only update keywords for this user
+	define("ONLY_USER", 65);    
+	
+	
+	// Only update keywords for this user
+	define("SCHEDULE", 'hourly');
