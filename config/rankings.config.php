b6d159bf84d703f83d7cf7fe797dfc41eb620cb3
josh <josh@6b763d2d-d01a-451f-aa8f-a39ca3e19446>
2011-07-26 18:47:18 +0000

    troubleshooting

--- config/rankings.config.php
+++ config/rankings.config.php
@@ -21,10 +21,9 @@
 	// ! Passed arguments through CLI ($argv[1] used for controller)              //
 	// ===========================================================================//  
 	
-
 	// Get passed server arguments
 	$argv = $_SERVER['argv'];
-	
+
 	// What search engine to scrape
 	define("ENGINE", $argv[2]);
 
--- controllers/workers/rankings.php
+++ controllers/workers/rankings.php
@@ -50,8 +50,6 @@ class rankings
 	
 	public function rankings($jobData)
 	{   
-
-		return true;
 		// Get the keywords from the job data				
 		$jobData = unserialize($jobData);
 
