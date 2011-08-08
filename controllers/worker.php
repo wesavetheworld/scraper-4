2119eac6f8a1bfe1362c52f7bfa306cea7dd636e
josh <josh@6b763d2d-d01a-451f-aa8f-a39ca3e19446>
2011-08-08 21:55:47 +0000

    version of new worker before trying to simplify it

--- controllers/worker.php
+++ controllers/worker.php
@@ -259,13 +259,11 @@ class worker
 		// Loop through each keyword
 		foreach($items as $key => &$item)
 		{  
-
-			print_r($item);
-			die();
 			// Generate the search page url 
-			//$item->setSearchUrl();			  		
+			$item->setSearchUrl();			  		
 			echo "item: ".$item->domain." | stat: ".$item->stat." url: ".$item->url."\n";
 			
+			die();
 			// If getting domain urls
 			if($this->model == "domains")
 			{ 			                     	
