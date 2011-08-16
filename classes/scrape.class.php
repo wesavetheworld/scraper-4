0cda64012a58558f951ef4a5641f68dd380dca52
josh <josh@6b763d2d-d01a-451f-aa8f-a39ca3e19446>
2011-08-16 22:50:39 +0000

    debugging info

--- classes/scrape.class.php
+++ classes/scrape.class.php
@@ -338,7 +338,9 @@ class scraper
 			// Attach any cUrl errors to output
 			$this->results[$i]['curlError'] = curl_error($this->ch[$i]);
 
+			print "curl error:\n"
 			print_r(curl_error($this->ch[$i]));
+			print "end of curl error\n\n";
 
 			// Set the output data
 			$this->results[$i]['output'] = curl_multi_getcontent($this->ch[$i]); 
