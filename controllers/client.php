a25aebe7fa5cc7eef5824ed031e7b2b5b8e113c1
josh <josh@6b763d2d-d01a-451f-aa8f-a39ca3e19446>
2011-08-22 22:11:15 +0000

    added max execution to workers back in 	needs testing

--- controllers/client.php
+++ controllers/client.php
@@ -126,7 +126,7 @@ class client
 					$data = serialize($data);
 					
 					// Define a new high priority job for current batch
-				   	$gmclient->addTaskBackground($task, $data, null, $task."_".$job++."_".time());						  		
+				   	$gmclient->addTask($task, $data, null, $task."_".$job++."_".time());						  		
 
 					// Create the jobs
 		    		$gmclient->runTasks();					
