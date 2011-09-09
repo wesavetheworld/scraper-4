ffb638e752fb0991d3d051a881372612a56e767f
josh <josh@6b763d2d-d01a-451f-aa8f-a39ca3e19446>
2011-09-09 22:24:59 +0000

    debugging.. turned off domain updates to see if thats whats crashing the script

--- controllers/worker.php
+++ controllers/worker.php
@@ -47,7 +47,7 @@ class worker
 //		require_once('classes/scrape.class.php'); 	
 
 		// Include proxy data model
-//		require_once('models/proxies.model.php'); 		
+		require_once('models/proxies.model.php'); 		
 		
 		// Reset benchmarking
 		//utilities::benchmark(false, false, false, true);		  		   	 			
