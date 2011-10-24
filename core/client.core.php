ee9ef12eed5a779192f98d439ccf003a20ef3add
josh <josh@6b763d2d-d01a-451f-aa8f-a39ca3e19446>
2011-10-24 22:28:14 +0000

    checking client core for timing issues

--- core/client.core.php
+++ core/client.core.php
@@ -248,10 +248,10 @@ class clientCore
 		// Execute command given
 		//exec($command);
 
+		file_put_contents("data/clientCore.log", "command: $controller $options ".date("r")."\n", FILE_APPEND);		
+
 		// Mimic live server exec command
 		sleep(rand(1, 55));
-
-		file_put_contents("data/clientCore.log", "command: $controller $options ".date("r")."\n", FILE_APPEND);		
 
 		// Log current command
 		utilities::notate("command: $controller $options", "clientd.log");		  		   	 				
