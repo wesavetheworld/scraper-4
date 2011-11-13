3d1d944f476c29da07b3f11f7edb4f6efa12cbf0
Joshua <thezenman@gmail.com>
2011-11-13 12:09:59 -0800

    proxy testing

--- models/proxies.model.php
+++ models/proxies.model.php
@@ -171,7 +171,7 @@ class proxies
 		{
 			echo "good\n";
 			// Create a random score to break up sequential adding
-			$score = time() + rand(0, 30);		
+			$score = time() - rand(0, 300000);		
 			echo "$score : $proxy\n";	
 		}
 		// Normal use
