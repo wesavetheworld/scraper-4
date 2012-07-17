306395dd03fa43aba9b67d82162993c61c928d0b
Joshua Heiland <thezenman@gmail.com>
2012-07-16 18:58:58 -0700

    increased curl timeout
    when anonymous proxies replaced their proxies they seemed to time out
    much much faster

--- classes/scrape.class.php
+++ classes/scrape.class.php
@@ -260,8 +260,8 @@ class scraper
 			//curl_setopt($this->ch[$i], CURLOPT_CONNECTTIMEOUT, CURL_TIMEOUT);
 			//curl_setopt($this->ch[$i], CURLOPT_TIMEOUT, 30);
 
-			curl_setopt($this->ch[$i], CURLOPT_CONNECTTIMEOUT, 5);
-			curl_setopt($this->ch[$i], CURLOPT_TIMEOUT, 5);			
+			curl_setopt($this->ch[$i], CURLOPT_CONNECTTIMEOUT, 30);
+			curl_setopt($this->ch[$i], CURLOPT_TIMEOUT, 30);			
 			
 			// Set random referer if array is supplied
 			if(!empty($this->referer))
